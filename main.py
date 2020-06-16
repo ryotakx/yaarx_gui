d78cbcac3c7f8c1200021fc10c39a63e47ca068b
weiran <liweiran97@gmail.com>
2020-06-17 01:54:12 +0800

    add config file

--- main.py
+++ main.py
@@ -1,42 +1,22 @@
 import sys
 import tools
-from config import choice_list, parameter_dict, input_types, description
+from tools import validator_equal_to, input_validator, configs
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, \
     QGridLayout, QVBoxLayout, QHBoxLayout, QComboBox, QMessageBox, QTextBrowser
-from PyQt5.QtGui import QIntValidator, QValidator, QRegExpValidator
-from PyQt5.QtCore import QRegExp
-
-
-hex_validator = QRegExpValidator(QRegExp('[0-9a-fA-F]{1,8}'))
-decimal_validator = QIntValidator(0, 2147483647)
-binary_validator = QRegExpValidator(QRegExp('[01]{1,16}'))
-validators = {'hex': hex_validator, 'decimal': decimal_validator, 'binary': binary_validator}
+from PyQt5.QtGui import QIcon
 
 
 class Demo(QWidget):
 
     def __init__(self):
         super(Demo, self).__init__()
-
+        self.setWindowTitle(configs.window_title)
+        self.setWindowIcon(QIcon(configs.window_logo))
         self.program_label = QLabel('Program:', self)
         self.program_combobox = QComboBox(self)
+        self.current_program = None
         self.description = QTextBrowser(self)
         self.description.setFixedHeight(80)
-        self.da_label = QLabel('da:', self)
-        self.db_label = QLabel('db:', self)
-        self.dc_label = QLabel('dc:', self)
-        self.da_line = QLineEdit(self)
-        self.db_line = QLineEdit(self)
-        self.dc_line = QLineEdit(self)
-        self.da_line.setText('0')
-        self.db_line.setText('0')
-        self.dc_line.setText('0')
-        self.da_line.setValidator(hex_validator)
-        self.db_line.setValidator(hex_validator)
-        self.dc_line.setValidator(hex_validator)
-        self.da_combobox = QComboBox(self)
-        self.db_combobox = QComboBox(self)
-        self.dc_combobox = QComboBox(self)
 
         self.compute_button = QPushButton('compute', self)
         self.compute_button.clicked.connect(self.compute_func)
@@ -51,21 +31,11 @@ class Demo(QWidget):
 
         self.layout_init()
         self.program_combobox_init()
-        self.input_combobox_init()
+        self.grid_layout_init()
 
     def layout_init(self):
         self.program_grid_layout.addWidget(self.program_label, 0, 0, 1, 1)
         self.program_grid_layout.addWidget(self.program_combobox, 0, 1, 1, 1)
-
-        self.grid_layout.addWidget(self.da_label, 0, 0, 1, 1)
-        self.grid_layout.addWidget(self.da_combobox, 0, 1, 1, 1)
-        self.grid_layout.addWidget(self.da_line, 0, 2, 1, 1)
-        self.grid_layout.addWidget(self.db_label, 1, 0, 1, 1)
-        self.grid_layout.addWidget(self.db_combobox, 1, 1, 1, 1)
-        self.grid_layout.addWidget(self.db_line, 1, 2, 1, 1)
-        self.grid_layout.addWidget(self.dc_label, 2, 0, 1, 1)
-        self.grid_layout.addWidget(self.dc_combobox, 2, 1, 1, 1)
-        self.grid_layout.addWidget(self.dc_line, 2, 2, 1, 1)
 
         self.v_layout.addLayout(self.program_grid_layout)
         self.v_layout.addWidget(self.description)
@@ -75,57 +45,46 @@ class Demo(QWidget):
 
         self.setLayout(self.v_layout)
 
+    def grid_layout_init(self):
+        self.on_combobox_change_layout()
+
     def program_combobox_init(self):
-        self.program_combobox.addItems(choice_list)
-        self.description.setText(description[self.program_combobox.currentText()])
-        # self.on_combobox_change_layout()
+        self.program_combobox.addItems(configs.choice_list)
         self.program_combobox.currentIndexChanged.connect(self.on_combobox_change_layout)
-    #    self.program_combobox.currentIndexChanged.connect(lambda: self.on_combobox_func(self.program_combobox))
-
-    #def on_combobox_func(self, combobox):
-    #    if combobox == self.program_combobox:
-    #        QMessageBox.information(self, 'Program_Combobox',
-    #                                '{}: {}'.format(combobox.currentIndex(), combobox.currentText()))
-
-    def input_combobox_init(self):
-        self.da_combobox.addItems(input_types)
-        self.db_combobox.addItems(input_types)
-        self.dc_combobox.addItems(input_types)
-        self.da_combobox.currentIndexChanged.connect(self.on_combobox_change_type)
-        self.db_combobox.currentIndexChanged.connect(self.on_combobox_change_type)
-        self.dc_combobox.currentIndexChanged.connect(self.on_combobox_change_type)
 
     def on_combobox_change_type(self):
+        length = configs.default_parameter_length
         for i in range(self.grid_layout.count()):
             widget = self.grid_layout.itemAt(i).widget()
             if isinstance(widget, QComboBox):
-                edit_line = self.grid_layout.itemAt(i+1).widget()
-                edit_line.setValidator(validators[widget.currentText()])
-                print(widget.currentText())
+                line = self.grid_layout.itemAt(i+1).widget()
+                for param in self.current_program.param:
+                    if param.name == self.grid_layout.itemAt(i-1).widget().text():
+                        length = param.length
+                new_validator = input_validator(length, widget.currentText())
+                if not validator_equal_to(new_validator, line.validator()):
+                    line.setValidator(new_validator)
+                    line.setText('0')
 
     def on_combobox_change_layout(self):
         program_name = self.program_combobox.currentText()
-        self.description.setText(description[program_name])
         self.text_browser.clear()
-        param_list = parameter_dict[program_name]
+        program = getattr(configs.programs, program_name)
+        self.current_program = program
+        self.description.setText(program.description)
+
         for i in range(self.grid_layout.count()):
             self.grid_layout.itemAt(i).widget().deleteLater()
-        for line_number, param in enumerate(param_list):
-            self.grid_layout.addWidget(QLabel(param + ':', self), line_number, 0, 1, 1)
-            combobox = QComboBox(self)
-            combobox.addItems(input_types)
-            combobox.currentIndexChanged.connect(self.on_combobox_change_type)
-            self.grid_layout.addWidget(combobox, line_number, 1, 1, 1)
-            line_edit = QLineEdit(self)
-            line_edit.setText('0')
-            line_edit.setValidator(hex_validator)
-            self.grid_layout.addWidget(line_edit, line_number, 2, 1, 1)
 
-    def input_validator(self):
-        for i in range(self.grid_layout.count()):
-            wig = self.grid_layout.itemAt(i).widget()
-            if isinstance(wig, QLineEdit):
-                print(wig.text())
+        for line_number, parameter in enumerate(program.param):
+            self.grid_layout.addWidget(QLabel(parameter.name + ':', self), line_number, 0, 1, 1)
+            combobox = QComboBox(self)
+            line_edit = QLineEdit(self)
+            self.grid_layout.addWidget(combobox, line_number, 1, 1, 1)
+            self.grid_layout.addWidget(line_edit, line_number, 2, 1, 1)
+            combobox.currentIndexChanged.connect(self.on_combobox_change_type)
+            combobox.addItems(configs.input_types)
+            combobox.setCurrentIndex(parameter.type)
 
     def compute_func(self):
         input_list = []
@@ -133,11 +92,10 @@ class Demo(QWidget):
             widget = self.grid_layout.itemAt(i).widget()
             if isinstance(widget, QLineEdit):
                 input_type = self.grid_layout.itemAt(i-1).widget().currentText()
-                input_list.append((input_type, widget.text()))
+                output_type = configs.input_types[self.current_program.param[i//3].type]
+                input_list.append(tools.conversion(input_type, widget.text(), output_type))
 
-        _, result, result_regex = tools.compute(self.program_combobox.currentText(), *tools.conversion(input_list))
-        if result_regex:
-            result = result + '\n\n=' + result_regex
+        result = tools.compute(self.current_program, configs.binary_file, *input_list)
         self.set_result_func(result)
 
     def set_result_func(self, result):
@@ -145,6 +103,7 @@ class Demo(QWidget):
 
 
 if __name__ == '__main__':
+    configs = configs()
     app = QApplication(sys.argv)
     demo = Demo()
     demo.show()
--- tools.py
+++ tools.py
@@ -1,9 +1,43 @@
 import subprocess
+import toml
+import re
+from PyQt5.QtGui import QIntValidator, QRegExpValidator
+from PyQt5.QtCore import QRegExp
 
 
-def compute(program_name, *param):
+class ConfigDict(dict):
+
+    def __init__(self, *args, **kwargs):
+        dict.__init__(self, *args, **kwargs)
+        self.__dict__ = self
+
+    def allow(self, state=True):
+        if state:
+            self.__dict__ = self
+        else:
+            self.__dict__ = dict()
+
+
+def configs():
+    f = open('config.toml', 'r')
+    config = toml.load(f, ConfigDict)
+    if config.choice_list == 'default':
+        config.choice_list = list(config.programs.keys())
+
+    for key, val in config.programs.items():
+        if 'output_lines' not in config.programs[key].keys():
+            config.programs[key]['output_lines'] = config.default_output_lines
+        for i, param in enumerate(config.programs[key].param):
+            if 'type' not in param.keys():
+                config.programs[key].param[i]['type'] = config.default_parameter_type
+            if 'length' not in param.keys():
+                config.programs[key].param[i]['length'] = config.default_parameter_length
+    return config
+
+
+def compute(program, addr, *param):
     run = subprocess.Popen(
-        './' + program_name,
+        addr + '/' + program.name,
         stdout=subprocess.PIPE,
         stdin=subprocess.PIPE,
         stderr=subprocess.PIPE,
@@ -13,22 +47,54 @@ def compute(program_name, *param):
     for i in param:
         run.stdin.write(i + '\n')
     run.stdin.close()
-
-    output = [str(line) for line in run.stdout]
-    print(output)
-    return_list = output[-2].strip().split('=')
-    if len(return_list) == 2:
-        return_list.append('')
-    return return_list
-
-
-def conversion(inputs):
     output = []
-    for param in inputs:
-        if param[0] == 'decimal':
-            output.append(hex(int(param[1]))[2:])
-        elif param[0] == 'binary':
-            output.append(hex(int(param[1], 2))[2:])
-        else:
-            output.append(param[1])
+    for line in run.stdout:
+        print(line)
+        output.append(str(line))
+    return prettify(output, program.output_lines)
+
+
+def prettify(output, lines):
+    return_list = []
+    for line in lines:
+        for i in re.split('[\]\[()]', output[line].strip().replace('=', '\n='))[2:]:
+            if i != '':
+                return_list.append(i.strip())
+        return_list.append('')
+    print(return_list)
+    return '\n'.join(return_list)
+
+
+def conversion(input_type, text, output_type):
+    output = text
+    if input_type == 'decimal' and output_type == 'hex':
+        output = hex(int(text))[2:]
+    if input_type == 'decimal' and output_type == 'binary':
+        output = bin(int(text))[2:]
+    if input_type == 'hex' and output_type == 'decimal':
+        output = str(int(text, 16))
+    if input_type == 'hex' and output_type == 'binary':
+        output = bin(int(text, 16))[2:]
+    if input_type == 'binary' and output_type == 'decimal':
+        output = str(int(text, 2))
+    if input_type == 'binary' and output_type == 'hex':
+        output = hex(int(text, 2))[2:]
     return output
+
+
+def input_validator(length, input_type):
+    if input_type == 'hex':
+        return QRegExpValidator(QRegExp('[0-9a-fA-F]{1,%i}' % length))
+    if input_type == 'decimal':
+        return QIntValidator(0, int('9' * length))
+    if input_type == 'binary':
+        return QRegExpValidator(QRegExp('[01]{1,%i}' % length * 4))
+
+
+def validator_equal_to(val1, val2):
+    if type(val1) == type(val2) == QRegExpValidator and val1.regExp() == val2.regExp():
+        return True
+    elif type(val1) == type(val2) == QIntValidator:
+        return True
+    else:
+        return False
